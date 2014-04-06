701ef79bcd9a13d9928ed0f2b0941af93775b1f6
Isabella Moreira <isabella@knights.ucf.edu>
2014-04-05 21:30:04 -0400

    user input

---
+++ app/src/main/java/com/evaluratr/app/ResultActivity.java
@@ -0,0 +1,20 @@
+package com.evaluratr.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
+/**
+ * Created by isabella on 4/5/14.
+ */
+public class ResultActivity extends Activity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.res_main);
+        //View donetwoButton = findViewById(R.id.donetwo_button);
+
+
+    }
+}
